58ae4215d19dbce7693ccaeaf1088221cb0038ab
Vixting <59802277+Vixting@users.noreply.github.com>
2024-02-19 13:30:00 +0000

    Drop down menus.

--- src/main/java/com/example/photoshop/Photoshop.java
+++ src/main/java/com/example/photoshop/Photoshop.java
@@ -5,35 +5,36 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.FileInputStream;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class Photoshop extends Application {
 
     private final ImageView imageView = new ImageView();
-    private final CheckBox nnCheckbox = new CheckBox("Use Nearest Neighbour");
-    private final Label interpolationLabel = new Label("Interpolation: Bilinear");
-    private final Slider gammaSlider = new Slider(0.1, 3.0, 1.0);
+    private final ComboBox<String> interpolationComboBox = new ComboBox<>();
+    private final ComboBox<String> filterComboBox = new ComboBox<>();
+    private final Slider gammaSlider = new Slider(0.1, 2.0, 1.0);
     private final Slider resizeSlider = new Slider(0.1, 2.0, 1.0);
-    private final Button laplacianButton = new Button("Toggle Laplacian Filter");
-    private boolean applyLaplacian = false;
+    private String currentInterpolationMethod = "Bilinear";
+    private String currentFilter = "None";
     private double lastScale = 1.0;
     private static final int[][] LAPLACIAN_FILTER = {
-            {-4,-1, 0,-1,-4},
-            {-1, 2, 3, 2,-1},
-            { 0, 3, 4, 3, 0},
-            {-1, 2, 3, 2,-1},
-            {-4,-1, 0,-1,-4}
+            {-4, -1, 0, -1, -4},
+            {-1, 2, 3, 2, -1},
+            {0, 3, 4, 3, 0},
+            {-1, 2, 3, 2, -1},
+            {-4, -1, 0, -1, -4}
     };
 
     public static void main(String[] args) {
@@ -65,136 +66,136 @@ public class Photoshop extends Application {
         Image originalImage = new Image(new FileInputStream("src/main/java/com/example/photoshop/raytrace.jpg"));
         imageView.setImage(originalImage);
 
-        Label gammaValueLabel = new Label("Gamma: 1.00"); // Initial value for gamma
-        Label resizeValueLabel = new Label("Resize: 1.00x"); // Initial value for resize
+        Label gammaValueLabel = new Label("Gamma: 1.00");
+        Label resizeValueLabel = new Label("Resize: 1.00x");
 
         setupSliderWithDebounce(gammaSlider, gammaValueLabel, originalImage);
         setupSliderWithDebounce(resizeSlider, resizeValueLabel, originalImage);
 
-        nnCheckbox.selectedProperty().addListener(e -> {
-            interpolationLabel.setText("Interpolation: " + (nnCheckbox.isSelected() ? "Nearest Neighbour" : "Bilinear"));
+        interpolationComboBox.getItems().addAll("Nearest Neighbor", "Bilinear");
+        interpolationComboBox.setValue("Bilinear");
+        interpolationComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+            currentInterpolationMethod = newValue;
             updateImage(originalImage);
         });
 
-        laplacianButton.setOnAction(e -> {
-            applyLaplacian = !applyLaplacian;
+        filterComboBox.getItems().addAll("None", "Laplacian");
+        filterComboBox.setValue("None");
+        filterComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
+            currentFilter = newValue;
             updateImage(originalImage);
         });
 
-        VBox root = new VBox(10); // added spacing
-        root.setPadding(new Insets(15, 20, 15, 20)); // added padding
+        HBox dropdownMenus = new HBox(10);
+        dropdownMenus.getChildren().addAll(
+                new Label("Interpolation Method"), interpolationComboBox,
+                new Label("Filter"), filterComboBox
+        );
+
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(15, 20, 15, 20));
         root.getChildren().addAll(
                 new Label("Gamma Correction"), gammaSlider, gammaValueLabel,
                 new Label("Resize Image"), resizeSlider, resizeValueLabel,
-                nnCheckbox, interpolationLabel,
-                laplacianButton, imageView
+                dropdownMenus,
+                imageView
         );
 
-        primaryStage.setScene(new Scene(root, 400, 600));
+        Scene scene = new Scene(root, 1300, 1300);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
     private void updateImage(Image originalImage) {
-        int width = (int) originalImage.getWidth(), height = (int) originalImage.getHeight();
-        WritableImage resized = new WritableImage((int) (width * lastScale), (int) (height * lastScale));
+        int originalWidth = (int) originalImage.getWidth();
+        int originalHeight = (int) originalImage.getHeight();
+        int newWidth = (int) (originalWidth * lastScale);
+        int newHeight = (int) (originalHeight * lastScale);
+        WritableImage resizedImage = new WritableImage(newWidth, newHeight);
         PixelReader reader = originalImage.getPixelReader();
-        PixelWriter writer = resized.getPixelWriter();
-        int newWidth = (int) resized.getWidth(), newHeight = (int) resized.getHeight();
+        PixelWriter writer = resizedImage.getPixelWriter();
 
         IntStream.range(0, newHeight).parallel().forEach(y -> {
             for (int x = 0; x < newWidth; x++) {
-                double scaleX = x / lastScale, scaleY = y / lastScale;
-                Color color = nnCheckbox.isSelected() ?
-                        getColorSafe(reader, (int) scaleX, (int) scaleY, width, height) :
-                        bilinearInterpolation(reader, scaleX, scaleY, width, height);
+                double scaleX = x / lastScale;
+                double scaleY = y / lastScale;
+                Color color;
+                if (currentInterpolationMethod.equals("Nearest Neighbor")) {
+                    color = getColorSafe(reader, (int) scaleX, (int) scaleY, originalWidth, originalHeight);
+                } else {
+                    color = bilinearInterpolate(reader, scaleX, scaleY, originalWidth, originalHeight);
+                }
                 synchronized (writer) {
                     writer.setColor(x, y, color);
                 }
             }
         });
 
-        Image processedImage = applyGammaCorrection(resized, gammaSlider.getValue());
-        if (applyLaplacian) {
+        Image processedImage = applyGammaCorrection(resizedImage, gammaSlider.getValue());
+        if (currentFilter.equals("Laplacian")) {
             processedImage = applyLaplacianFilter(processedImage);
         }
+
         Image finalProcessedImage = processedImage;
         Platform.runLater(() -> imageView.setImage(finalProcessedImage));
     }
 
-    private Color bilinearInterpolation(PixelReader reader, double x, double y, int maxWidth, int maxHeight) {
-        int xBase = (int) x, yBase = (int) y;
-        double xFraction = x - xBase, yFraction = y - yBase;
 
-        Color c00 = getColorSafe(reader, xBase, yBase, maxWidth, maxHeight),
-                c01 = getColorSafe(reader, xBase, yBase + 1, maxWidth, maxHeight),
-                c10 = getColorSafe(reader, xBase + 1, yBase, maxWidth, maxHeight),
-                c11 = getColorSafe(reader, xBase + 1, yBase + 1, maxWidth, maxHeight);
+    private Color bilinearInterpolate(PixelReader reader, double x, double y, int maxWidth, int maxHeight) {
+        int xFloor = (int) x, yFloor = (int) y;
+        double xFraction = x - xFloor, yFraction = y - yFloor;
 
-        return lerp(lerp(c00, c10, xFraction), lerp(c01, c11, xFraction), yFraction);
+        Color topLeft = getColorSafe(reader, xFloor, yFloor, maxWidth, maxHeight),
+                topRight = getColorSafe(reader, xFloor + 1, yFloor, maxWidth, maxHeight),
+                bottomLeft = getColorSafe(reader, xFloor, yFloor + 1, maxWidth, maxHeight),
+                bottomRight = getColorSafe(reader, xFloor + 1, yFloor + 1, maxWidth, maxHeight);
+
+        Color topInterpolated = linearInterpolate(topLeft, topRight, xFraction);
+        Color bottomInterpolated = linearInterpolate(bottomLeft, bottomRight, xFraction);
+
+        return linearInterpolate(topInterpolated, bottomInterpolated, yFraction);
     }
 
     private Color getColorSafe(PixelReader reader, int x, int y, int maxWidth, int maxHeight) {
-        if (x >= 0 && y >= 0 && x < maxWidth && y < maxHeight) return reader.getColor(x, y);
+        if (x >= 0 && y >= 0 && x < maxWidth && y < maxHeight) {
+            return reader.getColor(x, y);
+        }
         return Color.BLACK;
     }
 
-    private Color lerp(Color start, Color end, double ratio) {
+    private Color linearInterpolate(Color start, Color end, double fraction) {
         return new Color(
-                start.getRed() + ratio * (end.getRed() - start.getRed()),
-                start.getGreen() + ratio * (end.getGreen() - start.getGreen()),
-                start.getBlue() + ratio * (end.getBlue() - start.getBlue()), 1.0
+                start.getRed() + fraction * (end.getRed() - start.getRed()),
+                start.getGreen() + fraction * (end.getGreen() - start.getGreen()),
+                start.getBlue() + fraction * (end.getBlue() - start.getBlue()), 1.0
         );
     }
 
     private Image applyGammaCorrection(Image image, double gamma) {
         int width = (int) image.getWidth(), height = (int) image.getHeight();
-        WritableImage writableImage = new WritableImage(width, height);
+        WritableImage correctedImage = new WritableImage(width, height);
         PixelReader reader = image.getPixelReader();
-        PixelWriter writer = writableImage.getPixelWriter();
+        PixelWriter writer = correctedImage.getPixelWriter();
 
+        double inverseGamma = 1 / gamma;
         double[] gammaLUT = new double[256];
         for (int i = 0; i < 256; i++) {
-            gammaLUT[i] = Math.pow(i / 255.0, gamma);
+            gammaLUT[i] = Math.pow(i / 255.0, inverseGamma);
         }
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                Color color = reader.getColor(x, y);
+                Color originalColor = reader.getColor(x, y);
                 writer.setColor(x, y, new Color(
-                        gammaLUT[(int) (color.getRed() * 255)],
-                        gammaLUT[(int) (color.getGreen() * 255)],
-                        gammaLUT[(int) (color.getBlue() * 255)], 1.0
+                        gammaLUT[(int) (originalColor.getRed() * 255)],
+                        gammaLUT[(int) (originalColor.getGreen() * 255)],
+                        gammaLUT[(int) (originalColor.getBlue() * 255)], 1.0
                 ));
             }
         }
-        return writableImage;
+        return correctedImage;
     }
 
-
-    private Color crossCorrelate(PixelReader reader, int x, int y, int width, int height) {
-        double red = 0, green = 0, blue = 0;
-
-        for (int filterY = 0; filterY < 5; filterY++) {
-            for (int filterX = 0; filterX < 5; filterX++) {
-                int imageX = Math.min(Math.max(x - 2 + filterX, 0), width - 1);
-                int imageY = Math.min(Math.max(y - 2 + filterY, 0), height - 1);
-                Color pixelColor = reader.getColor(imageX, imageY);
-
-                int filterValue = LAPLACIAN_FILTER[filterY][filterX];
-                red += pixelColor.getRed() * filterValue;
-                green += pixelColor.getGreen() * filterValue;
-                blue += pixelColor.getBlue() * filterValue;
-            }
-        }
-
-        return new Color(clamp(red), clamp(green), clamp(blue), 1.0);
-    }
-
-    private double clamp(double value) {
-        if (value < 0.0) return 0.0;
-        return Math.min(value, 1.0);
-    }
 
     private Image applyLaplacianFilter(Image image) {
         int width = (int) image.getWidth();
@@ -211,5 +212,39 @@ public class Photoshop extends Application {
         }
 
         return result;
+    }
+
+    private Color crossCorrelate(PixelReader reader, int x, int y, int width, int height) {
+        double red = 0, green = 0, blue = 0;
+        int filterSize = LAPLACIAN_FILTER.length;
+
+        for (int filterY = 0; filterY < filterSize; filterY++) {
+            for (int filterX = 0; filterX < filterSize; filterX++) {
+                int imageX = x - filterSize / 2 + filterX;
+                int imageY = y - filterSize / 2 + filterY;
+
+                if (imageX < 0 || imageX >= width || imageY < 0 || imageY >= height) {
+                    continue;
+                }
+
+                Color pixelColor = reader.getColor(imageX, imageY);
+                int filterValue = LAPLACIAN_FILTER[filterY][filterX];
+
+                red += pixelColor.getRed() * filterValue;
+                green += pixelColor.getGreen() * filterValue;
+                blue += pixelColor.getBlue() * filterValue;
+            }
+        }
+
+        red = (red + 4) / 8;
+        green = (green + 4) / 8;
+        blue = (blue + 4) / 8;
+
+        return new Color(clamp(red), clamp(green), clamp(blue), 1.0);
+    }
+
+    private double clamp(double value) {
+        if (value < 0.0) return 0.0;
+        return Math.min(value, 1.0);
     }
 }
