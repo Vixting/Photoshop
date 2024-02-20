7364d9791ae6e4dcc227897a0b197ff72ed43303
Vixting <59802277+Vixting@users.noreply.github.com>
2024-02-20 10:07:36 +0000

    Zooming & Panning

--- src/main/java/com/example/photoshop/Photoshop.java
+++ src/main/java/com/example/photoshop/Photoshop.java
@@ -1,10 +1,5 @@
 package com.example.photoshop;
 
-import com.example.photoshop.filter.FilterFactory;
-import com.example.photoshop.filter.Filters;
-import com.example.photoshop.filter.GammaCorrectionFilter;
-import com.example.photoshop.interploators.Interpolator;
-import com.example.photoshop.interploators.InterpolatorFactory;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -12,17 +7,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.image.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import com.example.photoshop.filter.FilterFactory;
+import com.example.photoshop.filter.Filters;
+import com.example.photoshop.filter.GammaCorrectionFilter;
+import com.example.photoshop.interploators.Interpolator;
+import com.example.photoshop.interploators.InterpolatorFactory;
 import java.io.FileInputStream;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.IntStream;
 
 public class Photoshop extends Application {
 
@@ -31,6 +34,8 @@ public class Photoshop extends Application {
     private final ComboBox<String> filterComboBox = new ComboBox<>();
     private final Slider gammaSlider = new Slider(0.1, 5.0, 1.0);
     private final Slider resizeSlider = new Slider(0.1, 5.0, 1.0);
+    private double initialX;
+    private double initialY;
     private String currentInterpolationMethod = "Bilinear";
     private String currentFilter = "None";
     private final ExecutorService executorService = Executors.newSingleThreadExecutor();
@@ -40,25 +45,57 @@ public class Photoshop extends Application {
         launch(args);
     }
 
+    private double clamp(double value, double min, double max) {
+        if (value < min) return min;
+        return Math.min(value, max);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Mark's CS-256 application");
         Image originalImage = new Image(new FileInputStream("src/main/java/com/example/photoshop/raytrace.jpg"));
         imageView.setImage(originalImage);
-
         Label gammaValueLabel = new Label("Gamma: 1.00");
         Label resizeValueLabel = new Label("Resize: 1.00x");
-
         setupSliderWithDebounce(gammaSlider, gammaValueLabel, originalImage);
         setupSliderWithDebounce(resizeSlider, resizeValueLabel, originalImage);
-
         interpolationComboBox.getItems().addAll(InterpolatorFactory.getInterpolatorNames());
         interpolationComboBox.setValue("Bilinear");
         interpolationComboBox.valueProperty().addListener((observable, oldValue, newValue) -> {
             currentInterpolationMethod = newValue;
             updateImageAsync(originalImage);
         });
-
+        imageView.setOnMousePressed(event -> {
+            initialX = event.getX();
+            initialY = event.getY();
+        });
+        imageView.setOnMouseDragged(event -> {
+            double offsetX = event.getX() - initialX;
+            double offsetY = event.getY() - initialY;
+            double newTranslateX = imageView.getTranslateX() + offsetX;
+            double newTranslateY = imageView.getTranslateY() + offsetY;
+            double widthBound = Math.max(imageView.getBoundsInParent().getWidth() - imageView.getFitWidth(), 0) / 2;
+            double heightBound = Math.max(imageView.getBoundsInParent().getHeight() - imageView.getFitHeight(), 0) / 2;
+            newTranslateX = clamp(newTranslateX, -widthBound, widthBound);
+            newTranslateY = clamp(newTranslateY, -heightBound, heightBound);
+            imageView.setTranslateX(newTranslateX);
+            imageView.setTranslateY(newTranslateY);
+        });
+        imageView.setOnScroll(event -> {
+            double zoomFactor = 1.05;
+            double deltaY = event.getDeltaY();
+            if (deltaY < 0){
+                zoomFactor = 1 / zoomFactor;
+            }
+            double newScaleX = imageView.getScaleX() * zoomFactor;
+            double newScaleY = imageView.getScaleY() * zoomFactor;
+            double minScale = 0.5;
+            double maxScale = 5.0;
+            if (newScaleX >= minScale && newScaleX <= maxScale && newScaleY >= minScale && newScaleY <= maxScale) {
+                imageView.setScaleX(newScaleX);
+                imageView.setScaleY(newScaleY);
+            }
+        });
         filterComboBox.getItems().addAll("None");
         filterComboBox.setValue("None");
         filterComboBox.getItems().addAll(FilterFactory.getFilterNames());
@@ -66,13 +103,11 @@ public class Photoshop extends Application {
             currentFilter = newValue;
             updateImageAsync(originalImage);
         });
-
         HBox dropdownMenus = new HBox(10);
         dropdownMenus.getChildren().addAll(
                 new Label("Interpolation Method"), interpolationComboBox,
                 new Label("Filter"), filterComboBox
         );
-
         VBox root = new VBox(10);
         root.setPadding(new Insets(15, 20, 15, 20));
         root.getChildren().addAll(
@@ -81,7 +116,6 @@ public class Photoshop extends Application {
                 dropdownMenus,
                 imageView
         );
-
         Scene scene = new Scene(root, 1300, 1300);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style.css")).toExternalForm());
         primaryStage.setScene(scene);
@@ -103,10 +137,8 @@ public class Photoshop extends Application {
         if (lastTask != null && !lastTask.isDone()) {
             lastTask.cancel(true);
         }
-
         double currentScale = resizeSlider.getValue();
         double currentGamma = gammaSlider.getValue();
-
         lastTask = executorService.submit(() -> {
             int originalWidth = (int) originalImage.getWidth();
             int originalHeight = (int) originalImage.getHeight();
@@ -115,9 +147,7 @@ public class Photoshop extends Application {
             WritableImage resizedImage = new WritableImage(newWidth, newHeight);
             PixelReader reader = originalImage.getPixelReader();
             PixelWriter writer = resizedImage.getPixelWriter();
-
             Interpolator interpolator = InterpolatorFactory.createInterpolator(currentInterpolationMethod);
-
             for (int y = 0; y < newHeight; y++) {
                 for (int x = 0; x < newWidth; x++) {
                     double scaleX = (x / currentScale);
@@ -126,22 +156,18 @@ public class Photoshop extends Application {
                     writer.setColor(x, y, color);
                 }
             }
-
             Image finalImage = resizedImage;
             if (currentGamma != 1.0) {
                 finalImage = new GammaCorrectionFilter(currentGamma).applyFilter(finalImage);
             }
-
             if (!"None".equals(currentFilter)) {
                 Filters filter = FilterFactory.createFilter(currentFilter, currentGamma);
                 finalImage = filter.applyFilter(finalImage);
             }
-
             Image finalProcessedImage = finalImage;
             Platform.runLater(() -> imageView.setImage(finalProcessedImage));
         });
     }
-
 
     @Override
     public void stop() throws Exception {
