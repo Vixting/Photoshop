51c011a266a322a63ebece305199d5a2b83091e1
Vixting <59802277+Vixting@users.noreply.github.com>
2024-02-26 08:31:37 +0000

    Docs

--- src/main/java/com/example/photoshop/interploators/InterpolatorFactory.java
+++ src/main/java/com/example/photoshop/interploators/InterpolatorFactory.java
@@ -4,14 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * This class is a factory for creating interpolator instances.
+ * It maintains a map of interpolator names to their corresponding classes.
+ * It provides methods to create an interpolator instance and to get the names of all available interpolators.
+ */
 public class InterpolatorFactory {
+    // Map of interpolator names to their corresponding classes
     private static final Map<String, Class<? extends Interpolator>> interpolatorMap = new HashMap<>();
 
+    // Static initializer block to populate the interpolator map
     static {
         interpolatorMap.put("Nearest Neighbor", NearestNeighborInterpolator.class);
         interpolatorMap.put("Bilinear", BilinearInterpolator.class);
     }
 
+    /**
+     * This method creates an instance of the specified interpolator.
+     *
+     * @param interpolatorName The name of the interpolator to create an instance of.
+     * @return An instance of the specified interpolator.
+     * @throws RuntimeException if there is an error creating the interpolator instance.
+     */
     public static Interpolator createInterpolator(String interpolatorName) {
         try {
             return interpolatorMap.get(interpolatorName).getDeclaredConstructor().newInstance();
@@ -20,6 +34,11 @@ public class InterpolatorFactory {
         }
     }
 
+    /**
+     * This method returns the names of all available interpolators.
+     *
+     * @return A set of names of all available interpolators.
+     */
     public static Set<String> getInterpolatorNames() {
         return interpolatorMap.keySet();
     }
--- src/main/java/com/example/photoshop/interploators/NearestNeighborInterpolator.java
+++ src/main/java/com/example/photoshop/interploators/NearestNeighborInterpolator.java
@@ -3,8 +3,22 @@ package com.example.photoshop.interploators;
 import javafx.scene.image.PixelReader;
 import javafx.scene.paint.Color;
 
+/**
+ * This class represents a nearest neighbor interpolator.
+ * It implements the Interpolator interface and overrides the interpolate method to apply nearest neighbor interpolation.
+ */
 public class NearestNeighborInterpolator implements Interpolator {
 
+    /**
+     * This method applies the nearest neighbor interpolation to a pixel.
+     *
+     * @param reader The PixelReader of the image.
+     * @param x The x-coordinate of the pixel.
+     * @param y The y-coordinate of the pixel.
+     * @param maxWidth The maximum width of the image.
+     * @param maxHeight The maximum height of the image.
+     * @return The color of the nearest pixel.
+     */
     @Override
     public Color interpolate(PixelReader reader, double x, double y, int maxWidth, int maxHeight) {
         int nearestX = clamp((int) Math.round(x), 0, maxWidth - 1);
@@ -12,6 +26,14 @@ public class NearestNeighborInterpolator implements Interpolator {
         return reader.getColor(nearestX, nearestY);
     }
 
+    /**
+     * This method clamps a value between a minimum and maximum value.
+     *
+     * @param value The value to be clamped.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return The clamped value.
+     */
     private int clamp(int value, int min, int max) {
         return Math.max(min, Math.min(max, value));
     }
